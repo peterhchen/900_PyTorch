8875b22be490210abddf4d0dec7ce6011e4eb935
peterhchen <peter.hp.chen@gmail.com>
2020-05-21 22:15:38 -0700

    PyTorch Installation

---
+++ 03_install_pytorch/01_install_pytorch.py
@@ -0,0 +1,7 @@
+import torch
+print('\ntorch.__version__:')
+print(torch.__version__)
+print('\ntorch.cuda.is_available():')
+print(torch.cuda.is_available())
+print('\ntorch.version.cuda:')
+print(torch.version.cuda)
