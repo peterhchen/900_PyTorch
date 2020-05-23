f3ca8983f9356323345d33efc87c0466e0630da2
peterhchen <peter.hp.chen@gmail.com>
2020-05-22 21:32:14 -0700

    Add files via upload

---
+++ 08_create_tensor/03_create_tenstor_wo_data.py
@@ -0,0 +1,11 @@
+import torch
+import numpy as np
+
+print ('\ntorch.eye(2)')
+print (torch.eye(2))
+print('\ntorch.zeros(2,2):')
+print(torch.zeros(2,2))
+print('\ntorch.ones(2,2):')
+print(torch.ones(2,2))
+print('\ntorch.rand(2,2):')
+print(torch.rand(2,2))
