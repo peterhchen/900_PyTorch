4f3e0e0ad88514490a0fbf5e12204d7acd220a1d
peterhchen <peter.hp.chen@gmail.com>
2020-06-02 20:19:10 -0700

    COnfusion Matrix

---
+++ 27_confusion_matrix_src/03_build_confusion_matrix.py
@@ -0,0 +1,140 @@
+# Analyzing CNN Results - Building and Plotting a Confusion Matrix
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import torchvision
+import torchvision.transforms as transforms
+
+torch.set_printoptions(linewidth=120)
+
+print('torch.__version__',torch.__version__)
+print('torchvision.__version__', torchvision.__version__)
+
+def get_num_correct(preds, labels):
+    return preds.argmax(dim=1).eq(labels).sum().item()
+
+train_set = torchvision.datasets.FashionMNIST(
+    root='./data'
+    ,train=True
+    ,download=True
+    ,transform=transforms.Compose([
+        transforms.ToTensor()
+    ])
+)
+
+class Network(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=12, kernel_size=5)
+        
+        self.fc1 = nn.Linear(in_features=12*4*4, out_features=120)
+        self.fc2 = nn.Linear(in_features=120, out_features=60)
+        self.out = nn.Linear(in_features=60, out_features=10)
+        
+    def forward(self, t):
+        # (1) input layer
+        t = t
+        
+        # (2) hidden conv layer
+        t = self.conv1(t)
+        t = F.relu(t)
+        t = F.max_pool2d(t, kernel_size=2, stride=2)
+        
+        # (3) hidden conv layer
+        t = self.conv2(t)
+        t = F.relu(t)
+        t = F.max_pool2d(t, kernel_size=2, stride=2)
+        
+        # (4) hidden linear layer
+        t = t.reshape(-1, 12 * 4 * 4)
+        t = self.fc1(t)
+        t = F.relu(t)
+        
+        # (5) hidden linear layer
+        t = self.fc2(t)
+        t = F.relu(t)
+        
+        # (6) output layer
+        t = self.out(t)
+        #t = F.softmax(t, dim=1)
+        
+        return t
+
+torch.set_grad_enabled(False)
+network = Network()
+data_loader = torch.utils.data.DataLoader(train_set, batch_size=10)
+print('\nlen(train_set):', len(train_set))
+print('len(train_set.targets):', len(train_set.targets))
+# Getting predictions for the entire training set
+def get_all_preds(model, loader):
+    all_preds = torch.tensor([])
+    for batch in loader:
+        images, labels = batch
+
+        preds = model(images)
+        all_preds = torch.cat(
+            (all_preds, preds)
+            ,dim=0
+        )
+    return all_preds
+
+prediction_loader = torch.utils.data.DataLoader(train_set, batch_size=10000)
+train_preds = get_all_preds(network, prediction_loader)
+
+print('\ntrain_preds.shape:', train_preds.shape)
+print('train_preds.requires_grad:', train_preds.requires_grad)
+print('train_preds.grad:', train_preds.grad)
+print('train_preds.grad_fn:', train_preds.grad_fn)
+
+with torch.no_grad():
+    prediction_loader = torch.utils.data.DataLoader(train_set, batch_size=10000)
+    train_preds = get_all_preds(network, prediction_loader)
+
+print('\ntrain_preds.requires_grad:', train_preds.requires_grad)
+print('train_preds.grad:', train_preds.grad)
+print('train_preds.grad_fn:', train_preds.grad_fn)
+
+preds_correct = get_num_correct(train_preds, train_set.targets)
+
+print('total correct:', preds_correct)
+print('accuracy:', preds_correct / len(train_set))
+
+# Building a confusion matrix
+print('\ntrain_set.targets:', train_set.targets)
+print('train_preds.argmax(dim=1):', train_preds.argmax(dim=1))
+stacked = torch.stack(
+    (
+        train_set.targets
+        ,train_preds.argmax(dim=1)
+    )
+    ,dim=1
+)
+
+print('\nstacked.shape:', stacked.shape)
+print('stacked:')
+print(stacked)
+stacked[0].tolist()
+cmt = torch.zeros(10,10, dtype=torch.int32)
+print('\ncmt:')
+print(cmt)
+for p in stacked:
+    tl, pl = p.tolist()
+    cmt[tl, pl] = cmt[tl, pl] + 1
+print('\ncmt:')
+print(cmt)
+
+# Plotting a confusion matrix
+import matplotlib.pyplot as plt
+
+from sklearn.metrics import confusion_matrix
+from plotcm import plot_confusion_matrix
+
+cm = confusion_matrix(train_set.targets, train_preds.argmax(dim=1))
+print('\ntype(cm):', type(cm))
+print('cm:', cm)
+
+names = ('T-shirt/top','Trouser','Pullover','Dress','Coat','Sandal','Shirt','Sneaker','Bag','Ankle boot')
+plt.figure(figsize=(10,10))
+print('\nplot_confusion_matrix(cmt, names):', plot_confusion_matrix(cmt, names))
